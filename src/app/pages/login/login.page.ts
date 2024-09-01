521e02518e14c427ae4abe796d1d9d2952541ec7
diegoMejias20 <dieg.mejias@duocuc.cl>
2024-09-01 12:51:05 -0400

    1

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,12 @@ import {Router} from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
+  formularioLogin: FormGroup;
+
+
   admin:any = {
     usuario:'admin@gmail.com',
-    contraseña:'12341234'
+    password:'12341234'
 
   }
   
@@ -20,20 +25,41 @@ export class LoginPage implements OnInit {
     edad:25
   }
 
-  listaP: any =[
-    {
-      id:1,
-      comuna:''
-    }
-  ]
-  constructor(private router:Router) { }
+  constructor(private router:Router, public fb: FormBuilder, public alertController: AlertController, ) { 
+    
+    this.formularioLogin = this.fb.group({
+      'Email': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+  }
 
   ngOnInit() {
   }
+
   irRegistro(){
     this.router.navigate(['/registrar']);
   }
-  irPagina(){
+  
+  async irPagina(){
+    
+    var f = this.formularioLogin.value;
+    
+    var usuarioString = localStorage.getItem('usuario');
+    var usuario = usuarioString ? JSON.parse(usuarioString) : null;
+
+    if(usuario.Email == f.Email && usuario.password == f.password){
+      console.log('ingresado');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Los datos son incorrecots',
+        message: 'Tienes que rellenar los datos de los campos.',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+    }
+
+
     this.router.navigate(['/principal']);
   }
 
--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-registrar',
@@ -8,12 +11,45 @@ import {Router} from '@angular/router';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor(private router:Router) { }
+  formularioRegistro: FormGroup;
+
+  constructor(private router:Router, public fb: FormBuilder, public alertController:AlertController,) {
+    this.formularioRegistro = this.fb.group({
+      'Nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmacionPassword': new FormControl("",Validators.required),
+      'telefono': new FormControl("",Validators.required),
+      'Email': new FormControl("",Validators.required)
+    })
+   }
 
   ngOnInit() {
   }
-  irPagina(){
+  
+  async irPagina(){
+
     this.router.navigate(['/principal']);
+
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Tienes que rellenar los datos de los campos.',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+      return;
+    }
+    
+    var usuario = {
+      Email: f.Email,
+      password: f.password
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+
   }
 
 
